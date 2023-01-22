d8cad476d2db6db1b502abcb52289c0791ea1b33
adhirajkar3103 <adhirajkar3103@gmail.com>
2023-01-23 03:14:39 +0530

    Design changes

--- src/CoinListItem.js
+++ src/CoinListItem.js
@@ -6,7 +6,6 @@ export default function CoinListItem(props) {
   return (<>
     <li class="list-group-item d-flex justify-content-between" key={props.coin.id}>
      <img style={{width:'30px',height:'30px'}} src={props.coin.icon} alt='' /> 
-      <span>{props.coin.symbol}</span>
       <span> {props.coin.name}</span>  
       <span>${props.coin.price.toFixed(3)}</span>
      <button className='btn btn-dark' onClick={()=>navigate(`coin/${props.coin.id}`)}>
